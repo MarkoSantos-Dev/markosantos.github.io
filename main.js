d977e3bfcd81ec2787495484dcae38eec7a4a4ce
MarkoSantos-Dev <markosantos.dev@gmail.com>
2025-09-08 21:48:25 +0800

    debugging three js not working

--- main.js
+++ main.js
@@ -5,6 +5,16 @@ import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.m
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+window.addEventListener("DOMContentLoaded", () => {
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+});
+
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 });
